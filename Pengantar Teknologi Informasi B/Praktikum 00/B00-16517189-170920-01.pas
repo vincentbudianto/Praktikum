d23dd12c31cfd919bab645d4f30fa990465b545b
vincentbudianto <vincent.budianto@gmail.com>
2022-04-03 14:46:07 +0700

    Pengantar Teknologi Informasi B: + Praktikum 00

---
+++ Pengantar Teknologi Informasi B/Praktikum 00/B00-16517189-170920-01.pas
@@ -0,0 +1,15 @@
+{NIM / Nama : 16517189 / Vincent Budianto}
+{Nama File  : B00-16517189-170920-01.pas}
+{Tanggal    : 20 September 2017}
+{Deskripsi  : membuat program yang mengetikan 'Hello World!' pada layar}
+
+{Judul}
+program HelloWorld ;
+
+{Kamus}
+
+{Algoritma}
+begin
+   writeln ('Hello World!') ;
+   readln ;
+end.
